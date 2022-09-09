67905f028f018c28e6eab197d9706e39f859af63
snail <a595429717@163.com>
2022-09-09 23:36:18 +0800

    Initial commit

---
+++ Example/ZZWTool/ZZWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZZWAppDelegate.h
+//  ZZWTool
+//
+//  Created by a595429717@163.com on 09/09/2022.
+//  Copyright (c) 2022 a595429717@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZZWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
