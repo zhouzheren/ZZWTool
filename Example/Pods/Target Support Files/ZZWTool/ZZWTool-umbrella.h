8a605849bdd54727f17ba78ca4d029e965688691
snail <a595429717@163.com>
2022-09-13 15:42:34 +0800

    导入常用类，版本号升级到 0.1.0

--- Example/Pods/Target Support Files/ZZWTool/ZZWTool-umbrella.h
+++ Example/Pods/Target Support Files/ZZWTool/ZZWTool-umbrella.h
@@ -10,6 +10,17 @@
 #endif
 #endif
 
+#import "PurchaseInAppManager.h"
+#import "UIButton+ImageTitleSpacing.h"
+#import "UIButton+ZYFCustom.h"
+#import "UIColor+HexColor.h"
+#import "UIImage+Circle.h"
+#import "ZZWAnimation.h"
+#import "ZZWDismiss.h"
+#import "ZZWEncryption.h"
+#import "ZZWJSWeakDeleagete.h"
+#import "ZZWPresent.h"
+#import "ZZWTool.h"
 
 FOUNDATION_EXPORT double ZZWToolVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZZWToolVersionString[];
--- ZZWTool.podspec
+++ ZZWTool.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ZZWTool' #库的名字
-  s.version          = '0.0.1' #版本号，当私有库需要更新的时候只要修改这个值
+  s.version          = '0.1.0' #版本号，当私有库需要更新的时候只要修改这个值
   s.summary          = '基于第三方库的一些封装类' #库的简介，pod search 显示在上面的介绍
 
 # This description is used to generate tags and improve search results.
